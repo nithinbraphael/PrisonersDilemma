62ebbde4735d5bffc3e82d85e0e9fa508fd3be98
Mistrisen <nithinbraphael@gmail.com>
2026-02-25 12:37:35 +0530

    Custom payoff matrix support

--- main.cpp
+++ main.cpp
@@ -1,76 +1,69 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
-class Strategy{
-    public:
+struct Payoff {
+    int player1;
+    int player2;
+};
+
+struct Strategy{
     int points = 0;
-    vector<int> history;
-    bool (*run)(vector<int> history);
+    std::vector<int> history;
+    bool (*run)(std::vector<int> history);
 
     void display(){
-        cout << "Current points: " << points << endl;
-        cout << "Displaying history (C for Cooperate and D for Defect):" << endl;
+        std::cout << "Current points: " << points << std::endl;
+        std::cout << "Displaying history (C for Cooperate and D for Defect):" << std::endl;
         for (int action:history){
             char A;
             switch(action){
                 case 1: A = 'C'; break;
                 case 0: A = 'D'; break;
             }
-            cout << A << " ";
+            std::cout << A << " ";
         }
-        cout << "\n" << endl;
+        std::cout << "\n" << std::endl;
     }
 } s1, s2;
 
-bool S1Behaviour(vector<int> history){
+bool S1Behaviour(std::vector<int> history){
     //Modify strategy here, by default on tit for tat
     if (history.empty()) return 1;
     else return history.back();
 }
 
-bool S2Behaviour(vector<int> history){
+bool S2Behaviour(std::vector<int> history){
     //By default cooperates every round
     return 1;
 }
 
-void run(Strategy s1, Strategy s2, int noRounds){
+void run(Strategy s1, Strategy s2, int noRounds, Payoff matrix[2][2]){
     for (int round = 0; round < noRounds; round++){
-        if (s1.run(s2.history) == 1 && s2.run(s1.history) == 1){
-            s1.history.push_back(1);
-            s2.history.push_back(1);
-            s1.points += 10;
-            s2.points += 10;
-        }
-        else if (s1.run(s2.history) == 0 && s2.run(s1.history) == 1){
-            s1.history.push_back(0);
-            s2.history.push_back(1);
-            s1.points += 20;
-            s2.points -= 10;
-        }
-        else if (s1.run(s2.history) == 1 && s2.run(s1.history) == 0){
-            s1.history.push_back(1);
-            s2.history.push_back(0);
-            s1.points -= 10;
-            s2.points += 20;
-        }
-        else if (s1.run(s2.history) == 0 && s2.run(s1.history) == 0){
-            s1.history.push_back(0);
-            s2.history.push_back(0);
-            s1.points += 0;
-            s2.points += 0;
-        }
+        bool s1_value = s1.run(s2.history), s2_value = s2.run(s1.history);
+        s1.points += matrix[s1_value][s2_value].player1; s2.points += matrix[s1_value][s2_value].player2;
+        s1.history.push_back(s1_value); s2.history.push_back(s2_value);
     }
 
-    cout << "Strategy 1 Stats:" << endl;
+    std::cout << "Strategy 1 Stats:" << std::endl;
     s1.display();
-    cout << "Strategy 2 Stats:" << endl;
+    std::cout << "Strategy 2 Stats:" << std::endl;
     s2.display();
 }
 
 int main(){
     s1.run = S1Behaviour, s2.run = S2Behaviour;
     int noRounds;
-    cout << "Enter number of rounds: "; cin >> noRounds;
-    run(s1,s2,noRounds);
+    std::cout << "Enter number of rounds: "; std::cin >> noRounds;
+
+    Payoff matrix[2][2];
+    std::cout << "Enter payoff matrix values:\n(Format player1 player2 for each cell)\n";
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            std::cout << "Player1 " << (i == 1 ? "Cooperate" : "Defect") << ", Player2 " << (j == 1 ? "Cooperate" : "Defect") << " :";
+            std::cin >> matrix[i][j].player1 >> matrix[i][j].player2;
+        }
+    }
+
+    run(s1,s2,noRounds, matrix);
 }
